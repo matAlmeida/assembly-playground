c0e63f6bc5a3bf405e163ffa5fffcfb9a64b18ab
Matheus Almeida <mat.almeida@live.com>
2018-09-19 18:25:38 -0300

    NOT WORKING: trying to make a program that reads from stdin

---
+++ read-and-print/read.asm
@@ -0,0 +1,38 @@
+global _start
+
+section .data
+	userMsg: db 'Digite um numero: '
+	dispMsg: db 'O numero foi: '
+
+section .bss
+	num: resb 8
+
+section .text
+_start:
+	mov rax, 1 ;Syscall Write
+	mov rdi, 1 ;File Descriptor (stdout)
+	mov rsi, userMsg ;Message
+	mov rdx, 18 ;Message size
+	syscall
+
+	mov rax, 3 ;Syscall Read
+	mov rdi, 0 ;File Descriptor (stdin)
+	mov rsi, num
+	mov rdx, 8
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, dispMsg
+	mov rdx, 14
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, num
+	mov rdx, 8
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
