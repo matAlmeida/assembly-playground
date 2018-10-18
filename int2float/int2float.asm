adf318d33239109b38059d9c84235606ee891cd7
Matheus Almeida <mat.almeida@live.com>
2018-10-18 20:24:49 -0300

    changes

--- int2float/int2float.asm
+++ int2float/int2float.asm
@@ -6,6 +6,8 @@ SYS_READ equ 0	;Syscall
 SYS_WRITE equ 1	;Syscall
 SYS_EXIT equ 60	;Syscall
 
+STRING_TERMINATOR equ 0
+
 section .data
 	welcomeMsg: 	db 'Converter de Int para Float', 10
 	welcomeMsgLen:	equ $ - welcomeMsg
@@ -26,8 +28,14 @@ section .text
 	global _start
 
 	_start:
-		mov rax, 91
+		mov rax, 1234
 		call _print_uInt
+
+		mov rsi, string
+		mov rdx, 9
+		call _print
+
+		call _exit
 
 		mov rsi, inputMsg
 		mov rdx, inputMsgLen
@@ -66,17 +74,42 @@ section .text
 		jbe .next_digit			; Se for digito pula para o Next_Digit
 		ret						; Se não acabou os números válidos e retorna
 
-	_print_uInt:
+	int_to_string:
+		add rsi,9
+		mov byte [rsi],STRING_TERMINATOR
+
+		mov rbx,10
+		.next_digit:
+		xor rdx,rdx			; Clear rdx prior to dividing rdx:eax by rbx
+		div rbx				; rax /= 10
+		add dl,'0'			; Convert the remainder to ASCII
+		dec rsi				; store characters in reverse order
+		mov [rsi],dl
+		test rax,rax
+		jnz .next_digit		; Repeat until rax==0
+		mov rax,rsi
+		ret
+
+	_print_uInt:			; rax = 1234
 		mov rdi, 10
-		mov rbx, rax
-		div rdi
-		mul rdi
-		sub rbx, rax
-		div rdi
-		mov rsi, rax
-		mov rax, rbx
+		.redo:				; do {
+		mov rbx, rax		; rbx = 1234
+		div rdi				; rax = 1234 / 10 = 123
+		mul rdi				; rax = 123 * 10 = 1230
+		sub rbx, rax		; rbx = 1234 - 1230 = 4
+		div rdi				; rax = 1230 / 10 = 123
+		mov rsi, rax		; rsi = 123
+		mov rax, rbx		; rax = 4
+		call _printRAXDigit	; print '4'
+		mov rax, rsi		; rax = 123
+		cmp rax, 10
+		jge .redo			; } while (rax >= 10);
 		call _printRAXDigit
 		ret
+		; add rax, '0'
+		; mov [string], al
+		; mov rax, rbx
+
 
 	_printRAXDigit:
 		add rax, '0'
