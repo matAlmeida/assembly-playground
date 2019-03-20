3b6a32e59e0491ce499193dd8b64cf5beab31355
Matheus Almeida <mat.almeida@live.com>
2019-03-20 13:54:59 -0300

    feat: door closing

--- the-door/door.asm
+++ the-door/door.asm
@@ -33,6 +33,32 @@ section .data
 	doorOpening24:	db '  |---|                                                ', 10
 	doorOpeningLen:	equ $ - doorOpening1
 
+	doorClosing1:	db '----------------------            ---------------------', 10
+	doorClosing2:	db '     /     /    /   /|           /     /     /    /   /', 10
+	doorClosing3:	db '--------------------/|            -------------------  ', 10
+	doorClosing4:	db '    |     |     |  | |     -------------------    |  | ', 10
+	doorClosing5:	db '--------------------/|     |                 |-------  ', 10
+	doorClosing6:	db '      |      |     | |     |                 | |   |   ', 10
+	doorClosing7:	db '--------------------/|  x| |                 |-------  ', 10
+	doorClosing8:	db '    |     |     |  | |     |                 |   |   | ', 10
+	doorClosing9:	db '--------------------/      |                 |-------  ', 10
+	doorClosing10:	db '               fcf /       |0----()----()----|  fca    ', 10
+	doorClosing11:	db '                  /            /                       ', 10
+	doorClosing12:	db '                 /    <---    /                        ', 10
+	doorClosing13:	db '                /            /                         ', 10
+	doorClosing14:	db '               /            /                          ', 10
+	doorClosing15:	db '              /            /                           ', 10
+	doorClosing16:	db '             /            /                            ', 10
+	doorClosing17:	db '   / \                                                 ', 10
+	doorClosing18:	db '  /   \                                                ', 10
+	doorClosing19:	db '    |                                                  ', 10
+	doorClosing20:	db '    |                                                  ', 10
+	doorClosing21:	db '  |---|                                                ', 10
+	doorClosing22:	db '  | 0 | ba                                             ', 10
+	doorClosing23:	db '  | 0 | bf                                             ', 10
+	doorClosing24:	db '  |---|                                                ', 10
+	doorClosingLen:	equ $ - doorClosing1
+
 section .bss
 	num: resb 8
 
@@ -41,6 +67,10 @@ section .text
 	_start:
 		mov rsi, doorOpening1	;Message
 		mov rdx, doorOpeningLen	;Message size
+		call _print
+		
+		mov rsi, doorClosing1	;Message
+		mov rdx, doorClosingLen	;Message size
 		call _print
 
 		call _exit
