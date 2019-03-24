ff8724c6077968699a7f06bb49a9591ebc445a34
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-03-24 13:50:12 -0300

    Adcionada selecao do estado do portao e feito mock de fechado e aberto. Criada funcao para comparacao e impressao do portao

--- the-door/door.asm
+++ the-door/door.asm
@@ -71,6 +71,32 @@ section .data
 	doorOpening24:	db '  |---|                                                ', 10
 	doorOpeningLen:	equ $ - doorOpening1
 
+	doorOpened1:	db '----------------------            ---------------------', 10
+	doorOpened2:	db '     /     /    /   /|           /     /     /    /   /', 10
+	doorOpened3:	db '--------------------/|            -------------------  ', 10
+	doorOpened4:	db '    |     |     |  | |     -------------------    |  | ', 10
+	doorOpened5:	db '--------------------/|     |                 |-------  ', 10
+	doorOpened6:	db '      |      |     | |     |                 | |   |   ', 10
+	doorOpened7:	db '--------------------/|  x| |                 |-------  ', 10
+	doorOpened8:	db '    |     |     |  | |     |                 |   |   | ', 10
+	doorOpened9:	db '--------------------/      |                 |-------  ', 10
+	doorOpened10:	db '               fcf /       |0----()----()----|  fca    ', 10
+	doorOpened11:	db '                  /            /                       ', 10
+	doorOpened12:	db '                 /    --->    /                        ', 10
+	doorOpened13:	db '                /            /                         ', 10
+	doorOpened14:	db '               /            /                          ', 10
+	doorOpened15:	db '              /            /                           ', 10
+	doorOpened16:	db '             /            /                            ', 10
+	doorOpened17:	db '   / \                                                 ', 10
+	doorOpened18:	db '  /   \                                                ', 10
+	doorOpened19:	db '    |                                                  ', 10
+	doorOpened20:	db '    |                                                  ', 10
+	doorOpened21:	db '  |---|                                                ', 10
+	doorOpened22:	db '  | 0 | ba                                             ', 10
+	doorOpened23:	db '  | 0 | bf                                             ', 10
+	doorOpened24:	db '  |---|                                                ', 10
+	doorOpenedLen:	equ $ - doorOpened1
+
 	doorClosing1:	  db '----------------------            ---------------------', 10
 	doorClosing2:	  db '     /     /    /   /|           /     /     /    /   /', 10
 	doorClosing3:	  db '--------------------/|            -------------------  ', 10
@@ -97,7 +123,36 @@ section .data
 	doorClosing24:	db '  |---|                                                ', 10
 	doorClosingLen:	equ $ - doorClosing1
 
+	doorClosed1:	db '----------------------            ---------------------', 10
+	doorClosed2:	db '     /     /    /   /|           /     /     /    /   /', 10
+	doorClosed3:	db '--------------------/|            -------------------  ', 10
+	doorClosed4:	db '    |     |     |  | |     -------------------    |  | ', 10
+	doorClosed5:	db '--------------------/|     |                 |-------  ', 10
+	doorClosed6:	db '      |      |     | |     |                 | |   |   ', 10
+	doorClosed7:	db '--------------------/|  x| |                 |-------  ', 10
+	doorClosed8:	db '    |     |     |  | |     |                 |   |   | ', 10
+	doorClosed9:	db '--------------------/      |                 |-------  ', 10
+	doorClosed10:	db '               fcf /       |0----()----()----|  fca    ', 10
+	doorClosed11:	db '                  /            /                       ', 10
+	doorClosed12:	db '                 /    <---    /                        ', 10
+	doorClosed13:	db '                /            /                         ', 10
+	doorClosed14:	db '               /            /                          ', 10
+	doorClosed15:	db '              /            /                           ', 10
+	doorClosed16:	db '             /            /                            ', 10
+	doorClosed17:	db '   / \                                                 ', 10
+	doorClosed18:	db '  /   \                                                ', 10
+	doorClosed19:	db '    |                                                  ', 10
+	doorClosed20:	db '    |                                                  ', 10
+	doorClosed21:	db '  |---|                                                ', 10
+	doorClosed22:	db '  | 0 | ba                                             ', 10
+	doorClosed23:	db '  | 0 | bf                                             ', 10
+	doorClosed24:	db '  |---|                                                ', 10
+	doorClosedLen:	equ $ - doorClosed1
+
 	askState: db 'Digite 1 para abrir o portão e 2 para fechar', 10
+	askStateLen: equ $ - askState
+
+	state: db 0
 
 section .bss
 	num: resb 8
@@ -105,14 +160,29 @@ section .bss
 section .text
 	global _start
 	_start:
+		print askState, askStateLen
+		scan state, 1
+		call _chooseState
 
+		exit
 
-		; mov rsi, doorOpening1	;Message
-		; mov rdx, doorOpeningLen	;Message size
-		; call _print
-
-		; mov rsi, doorClosing1	;Message
-		; mov rdx, doorClosingLen	;Message size
-		; call _print
-
-		; call _exit
+	; Input:
+	;		r15 - estado desejado
+	;		r14 - 1 (em ascii)
+	; Output:
+	;		Estado de transicao do portao e estado final
+	_chooseState:
+		mov r15, [state]											; estado é passado para r15
+		mov r14, 49														; num 1(ascii) é passado para r14
+		cmp r15, r14													; comparacao entre o estado passado e 1
+		je .openDoor													; se o estado passado for 1, abre o portao
+		jmp .closeDoor												; se nao for, fecha o portao
+		.openDoor:														; abertura
+			print doorOpening1, doorOpeningLen
+			print doorOpened1, doorOpenedLen
+			ret
+		.closeDoor:														; fechamento
+			print doorClosing1, doorClosingLen
+			print doorClosed1, doorClosedLen
+			ret
+		ret
