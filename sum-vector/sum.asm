8cb034d133d5f78010ca79f5fcbea5985f696663
Matheus Almeida <mat.almeida@live.com>
2019-04-01 05:30:29 -0300

    feat: new sum vector exercise

---
+++ sum-vector/sum.asm
@@ -0,0 +1,58 @@
+STDIN equ 0
+STDOUT equ 1
+STDERR equ 2
+
+SYS_READ equ 0	;Syscall
+SYS_WRITE equ 1	;Syscall
+SYS_EXIT equ 60	;Syscall
+
+	; Output:
+	;		Finaliza a execução do programa
+%macro exit 0
+    mov rax, SYS_EXIT   ; system call for exit
+    xor rdi, rdi        ; exit code 0
+    syscall             ; invoke operating system to exit
+%endmacro
+
+	; Input:
+	;		RSI - Buffer
+	;		RDX - Count
+	; Output:
+	;		Printa conteúdo do Buffer na tela
+%macro print 2
+
+    mov rax, SYS_WRITE  ; systemcall for write
+    mov rdi, STDOUT     ; file handle 1 is stdout
+    mov rsi, %1         ; address of string to output
+    mov rdx, %2         ; number of bytes
+    syscall
+
+%endmacro
+
+	; Input:
+	;		RSI - Buffer
+	;		RDX - Count
+	; Output:
+	;		Salva input do úsuario no Buffer
+%macro scan 2
+
+    mov rax, SYS_READ   ; systemcall for read
+    mov rdi, STDIN     ; file handle 1 is stdout
+    mov rsi, %1         ; address of string to output
+    mov rdx, %2         ; number of bytes
+    syscall
+
+%endmacro
+
+section .data
+    teste:      db 'OMG', 10
+    testeLen:   equ $ - teste
+
+section .bss
+	num: resb 8
+
+section .text
+	global _start
+	_start:
+		print teste, testeLen
+        exit
