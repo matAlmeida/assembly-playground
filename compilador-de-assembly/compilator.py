6db48348e79da2079ba82d92804412dc4714d0a8
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-07-29 09:27:26 -0300

    :sparkles: read instructions and registers

--- compilador-de-assembly/compilator.py
+++ compilador-de-assembly/compilator.py
@@ -2,16 +2,30 @@
 import re
 
 filename = "hello.asm"
+labels = []
+inst = []
+regs = []
+instructions = ['add', 'mov', 'xor', 'syscall']
+registers = ['rax', 'rdi', 'rsi', 'rdx']
+# Make a regex that matches if any of our regexes match.
+instCombined = "(" + ")|(".join(instructions) + ")"
+regCombined = "(" + ")|(".join(registers) + ")"
 
 with open(filename) as f:
     content = [word for line in f for word in line.split()]
 
-labels = []
-print(content)
+# print(content)
 
 for word in content:
-    label = re.search('.+:', word)
+
+    label = re.match('.+:', word)
+    instructions = re.match(instCombined, word)
+    registers = re.match(regCombined, word)
     if label != None:
         labels.append(label.string)
+    if instructions != None:
+        inst.append(instructions.string)
+    if registers != None:
+        regs.append(registers.string)
 
-print(labels)
+print(regs)
