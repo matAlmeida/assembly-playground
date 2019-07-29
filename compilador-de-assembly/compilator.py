a5673ab5b2af1b7a64308aa09d297d24866c3bf6
Matheus Almeida <mat.almeida@live.com>
2019-07-29 10:28:12 -0300

    feat(compilator): add support for section and variables

--- compilador-de-assembly/compilator.py
+++ compilador-de-assembly/compilator.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 import re
 
-filename = "hello.asm"
+filename = "door.asm"
 labels = []
 inst = []
 regs = []
-instructions = ['add', 'mov', 'xor', 'syscall']
-registers = ['rax', 'rdi', 'rsi', 'rdx']
 # Make a regex that matches if any of our regexes match.
+instructions = ['add', 'mov', 'xor', 'syscall', 'print',
+                'call', 'ret', 'cmp', 'je', 'jmp', 'jg', 'movzx']
 instCombined = "(" + ")|(".join(instructions) + ")"
+registers = ['rax', 'rdi', 'rsi', 'rdx',
+             'r15', 'r14', 'r13', 'r12', 'r11', 'r10']
 regCombined = "(" + ")|(".join(registers) + ")"
 
 with open(filename) as f:
-    content = [word for line in f for word in line.split()]
+    for line in f:
+        line = line.split(';')[0]
 
-# print(content)
-
-for word in content:
-
-    label = re.match('.+:', word)
-    instructions = re.match(instCombined, word)
-    registers = re.match(regCombined, word)
-    if label != None:
-        labels.append(label.string)
-    if instructions != None:
-        inst.append(instructions.string)
-    if registers != None:
-        regs.append(registers.string)
-
-print(regs)
+        label = re.match('.+:\n', line)
+        instructions = re.search(instCombined, line)
+        registers = re.search(regCombined, line)
+        section = re.search('^section \..+', line)
+        variable = re.search('^\s*\w+:\s*.+\n', line)
+        if label != None:
+            labelString = label.string
+            labelString = labelString.replace('\n', '')
+            labelString = labelString.replace('\t', '')
+            labelString = labelString.replace(' ', '')
+            labels.append(labelString)
+            print('<LABEL>', labelString)
+        if instructions != None:
+            inst.append(instructions)
+            print('<INST>', instructions.group(0))
+        if registers != None:
+            regs.append(registers.string)
+            print('<REGS>', registers.group(0))
+        if section != None:
+            print('<SECTION>', section.group(0))
+        if variable != None:
+            print('<VARIABLE>', variable.group(0)[:-1])
